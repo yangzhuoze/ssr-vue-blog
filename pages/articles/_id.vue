d980ea197526af471e9256e3972f12d7f1dd857d
Yang Zhuoze <yangzhuoze@163.com>
2018-06-04 01:19:41 +0800

    Initial commit

---
+++ pages/articles/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <h1>{{ article.title }}</h1>
+    <p>Created: {{ article.created_time }}</p>
+    <p>Modified: {{ article.modified_time }}</p>
+    <div v-html="article.body"></div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    let { data } = await axios.get(`http://127.0.0.1:8000/blog/articles/${params.id}/`)
+    return {
+      article: data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/tags/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <h1>{{ tag.name }}</h1>
+    <ul>
+      <li v-for="(article, index) in tag.articles" :key="index">
+        {{ article.title }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    let { data } = await axios.get(`http://127.0.0.1:8000/blog/tags/${params.name}/`)
+    return {
+      tag: data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
