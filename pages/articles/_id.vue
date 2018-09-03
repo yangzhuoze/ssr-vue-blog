fa8ecadd181f860d8148a10f6df0c5dc1663512c
Yang Zhuoze <yangzhuoze@163.com>
2018-09-04 00:39:38 +0800

    Basically cocoa theme in Article Detail page

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,23 +1,36 @@
 <template>
-  <section class="container">
-    <h1>{{ article.title }}</h1>
-    <p>Created: {{ article.created_time }}</p>
-    <p>Modified: {{ article.modified_time }}</p>
-    <div v-html="article.body"></div>
-  </section>
+  <div class="cocoa-content">
+    <div class="front-matter">
+      <div class="title-container">
+        <div class="page-heading">{{ article.title }}</div>
+      </div>
+      <div class="cocoa-meta">
+        <div class="cocoa-date" :title="article.created_time">
+          Created：{{ article.created_time | moment().format("ll") }}
+        </div>
+        <div>
+          <div class="middot"></div>
+          <div class="cocoa-date" :title="article.modified_time">
+            Modified：{{ article.modified_time | moment().format("ll") }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cocoa-markdown" v-html="article.body"></div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
 
-export default {
-  async asyncData({ params }) {
-    let { data } = await axios.get(`http://127.0.0.1:8000/blog/articles/${params.id}/`)
-    return {
-      article: data
+  export default {
+    async asyncData({params}) {
+      let {data} = await axios.get(`http://127.0.0.1:8000/blog/articles/${params.id}/`)
+      return {
+        article: data
+      }
     }
   }
-}
 </script>
 
 <style scoped>
